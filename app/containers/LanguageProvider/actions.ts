c492a96fceb5ba76eff0923a1a330becc958c4c2
Gustavo Silva <gustavorssilva09@gmail.com>
2020-11-17 11:32:45 +0000

    [new] init boilerplate

---
+++ app/containers/LanguageProvider/actions.ts
@@ -0,0 +1,10 @@
+/*
+ *
+ * LanguageProvider actions
+ *
+ */
+
+import ActionTypes from './constants';
+import { action } from 'typesafe-actions';
+
+export const changeLocale = (languageLocale: string) => action(ActionTypes.CHANGE_LOCALE, languageLocale);
