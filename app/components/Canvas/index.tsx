9b9491e7572a1f695b8c3293a947bf36f55eb7f5
Gustavo Silva <gustavorssilva09@gmail.com>
2020-12-10 18:35:06 +0000

    [new] canvas

--- app/components/Canvas/index.tsx
+++ app/components/Canvas/index.tsx
@@ -9,14 +9,40 @@ import { Point } from 'types/common';
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
 
+import CanvasJSReact from 'assets/canvajs/canvasjs.react';
+var CanvasJS = CanvasJSReact.CanvasJS;
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
 interface Props {
     points: Point[];
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function Canvas(props: Props) {
-    // console.log(props.points);
-    return <div>yooo</div>;
+        const options = {
+          title: {
+            text: "Basic Column Chart in React"
+          },
+          data: [{				
+                    type: "column",
+                    dataPoints: [
+                        { label: "Apple",  y: 10  },
+                        { label: "Orange", y: 15  },
+                        { label: "Banana", y: 25  },
+                        { label: "Mango",  y: 30  },
+                        { label: "Grape",  y: 28  }
+                    ]
+           }]
+       }
+            
+       return (
+          <div>
+            <CanvasJSChart options = {options}
+                /* onRef = {ref => this.chart = ref} */
+            />
+          </div>
+        );
+      }
 }
 
 export default Canvas;
